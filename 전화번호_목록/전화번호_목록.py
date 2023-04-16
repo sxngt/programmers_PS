4b7c9c01880393d1f35fa49497e71ac958eb1c4f
ysh <qwaszx8614@gmail.com>
2023-04-16 20:39:43 +0900

    정민지와 정상결전

---
+++ 전화번호_목록/전화번호_목록.py
@@ -0,0 +1,10 @@
+def solution(phoneBook):
+    phoneBook = sorted(phoneBook)
+
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+
+
+print(solution(["119", "97674223", "1195524421"]))
